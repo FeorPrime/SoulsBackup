f9238bcde49f4e2ad154b6fbc4f91f41723f6126
Feor <5939320+FeorPrime@users.noreply.github.com>
2025-03-05 00:53:15 +0300

    MenuItem extracted into separate file. May be it's time to stop growing my own bicycle and start to use some cli framework instead 🥲

---
+++ SoulsBackup/MenuItem.cs
@@ -0,0 +1,17 @@
+﻿namespace SoulsBackup;
+
+public record MenuItem
+{
+    private static int _count = 1;
+
+    public int Id { get; init; } = _count++;
+
+    public required string Command { get; init; }
+    public Action<string>? Action { get; set; }
+    
+    public string Description { get; init; } = string.Empty;
+    
+    public string Shortcut { get; init; } = string.Empty;
+    
+    public string Doc { get; init; } = string.Empty;
+}
--- SoulsBackup/Program.cs
+++ SoulsBackup/Program.cs
@@ -1,75 +1,95 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.Reflection;
 using SoulsBackup;
+using static System.Int32;
+
+const string NOT_IMPLEMENTED_YET = "Not implemented yet";
 
 ConsoleHelpers.PrintHeader("SoulsBackup");
 
 var menu = GetMenu();
+menu.FirstOrDefault(x => x.Id == 5)!.Action = s => PrintMenu(menu, s);
+
 PrintMenu(menu);
 while (true)
 {
     ConsoleHelpers.PrintInfo("Enter a command: ");
-    var commandWithArguments  = Console.ReadLine()?.Split(' ');
+    var commandWithArguments  = Console.ReadLine();
     
-    var command = commandWithArguments?.FirstOrDefault() ?? "help";
-    var argument = commandWithArguments?.Skip(1).FirstOrDefault() ?? string.Empty;
+    var command = commandWithArguments?[..commandWithArguments.IndexOf(' ')] ?? "help";
+    var arguments = commandWithArguments?[commandWithArguments.IndexOf(' ')..] ?? string.Empty;
+
+    _ = TryParse(command, out var menuItem);
     
-    if( int.TryParse(command, out var menuItem) )
+    var callback = menu.FirstOrDefault(x => x.Id == menuItem || x.Command == command || x.Shortcut == command)?.Action;
+    if (callback == null)
     {
-        var item = menu.FirstOrDefault(x => x.Id == menuItem);
-        item?.Action(string.Empty);
+        ConsoleHelpers.PrintError($"Action for {command} is not set, or command is not found");
         ConsoleHelpers.PrintLine();
         continue;
     }
-    
-    menu.FirstOrDefault(x => x.Command == command || x.Shortcut == command)?.Action(argument);
+    callback(arguments);
     ConsoleHelpers.PrintLine();
 }
 
-static HashSet<MenuItem> GetMenu() => [
-    new() { Id = 1, Command = "config", Action = _ => Config(), Description = "Configure the backup", Shortcut = "c" },
-    new() { Id = 2,  Command = "list", Action = _ => List(), Description = "List available backups", Shortcut = "l"},
-    new() { Id = 3,  Command = "backup", Action = Backup, Description = "Backup to a snapshot", Shortcut = "b" },
-    new() { Id = 4,  Command = "restore", Action = Restore, Description = "Restore from the snapshot", Shortcut = "r" },
-    new() { Id = 5,  Command = "help", Action = (c) => PrintMenu(GetMenu(),c), Description = "Print this", Shortcut = "?"},
-    new() { Id = 6,  Command = "exit", Action = _ => Environment.Exit(0), Description = "Exit the program", Shortcut = "e" },
-];
+static HashSet<MenuItem> GetMenu()
+{
+    var assemblyName = Assembly.GetExecutingAssembly().GetName().Name!;
+    return
+    [
+        new()
 
-static void  PrintMenu(HashSet<MenuItem> menu,string command = "")
+        {
+            Id = 1, Command = "config",
+            Action = Config, Description = "Configure the backup", Shortcut = "c",
+            Doc = $"Usage: {assemblyName} config path/to/config\n Shows current configuration settings and let you change them"
+        },
+        new()
+        {
+            Id = 2, Command = "list",
+            Action = List, Description = "List available backups", Shortcut = "l",
+            Doc = $"Usage: {assemblyName} list\n Shows all saved backup snapshots"
+        },
+        new()
+        {
+            Id = 3, Command = "backup",
+            Action = Backup, Description = "Backup to a snapshot", Shortcut = "b", 
+            Doc = $"Usage: {assemblyName} backup <snapshot>\n Saves backup snapshot"
+        },
+        new()
+        {
+            Id = 4, Command = "restore",
+            Action = Restore, Description = "Restore from the snapshot", Shortcut = "r", 
+            Doc = $"Usage: {assemblyName} restore <snapshot>\n Restore saved backup snapshot"
+        },
+        new()
+        {
+            Id = 5, Command = "help",
+            Action = null, Description = "Print this", Shortcut = "?", 
+            Doc = $"Usage: {assemblyName} help\n Shows help text. Can print help for any command in the list"
+        },
+        new()
+        {
+            Id = 6, Command = "exit",
+            Action = _ => Environment.Exit(0), Description = "Exit the program", Shortcut = "e", 
+            Doc = $"Usage: {assemblyName} exit\n ???"
+        },
+    ];
+}
+
+static void  PrintMenu(HashSet<MenuItem> menu, string command = "")
 {
     ConsoleHelpers.PrintInfo("Available commands:");
-    
+    if (command != "")
+    {
+        ConsoleHelpers.PrintInfo($"Command: {command}\n{menu.FirstOrDefault(x => x.Command == command)?.Doc}");
+    }
     foreach (var menuItem in menu)
     {
         ConsoleHelpers.PrintInfo($"{menuItem.Id} - {menuItem.Command} \t-{menuItem.Shortcut} \t: {menuItem.Description}");
     }
 }
 
-static void Config() => ConsoleHelpers.PrintInfo("Not implemented yet");
-static void List() => ConsoleHelpers.PrintInfo("Not implemented yet");
-static void Backup(string snapshotName) => ConsoleHelpers.PrintInfo("Not implemented yet");
-static void Restore(string snapshotName) => ConsoleHelpers.PrintInfo("Not implemented yet");
-public record MenuItem
-{
-    private static int _count = 1;
-    public MenuItem()
-    {
-    }
-
-    [SetsRequiredMembers]
-    public MenuItem(string command, Action<string> action, string description = "", string shortcut = "")
-    {
-        Command = command;
-        Action = action;
-        Description = description;
-        Shortcut = shortcut;
-    }
-
-    public int Id { get; init; } = _count++;
-
-    public required string Command { get; init; }
-    public required Action<string> Action { get; init; }
-    
-    public string Description { get; init; } = string.Empty;
-    
-    public string Shortcut { get; init; } = string.Empty;
-}
+static void Config(string _) => ConsoleHelpers.PrintError(NOT_IMPLEMENTED_YET);
+static void List(string _) => ConsoleHelpers.PrintError(NOT_IMPLEMENTED_YET);
+static void Backup(string snapshotName) => ConsoleHelpers.PrintError(NOT_IMPLEMENTED_YET);
+static void Restore(string snapshotName) => ConsoleHelpers.PrintError(NOT_IMPLEMENTED_YET);
